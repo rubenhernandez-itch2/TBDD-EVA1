e17c224a889701cc5122a0d5720791d2419248d8
rubenhernandez-itch2 <81659471+rubenhernandez-itch2@users.noreply.github.com>
2025-08-26 17:02:32 -0600

    Add files via upload

---
+++ EVA1_1_HOLA_MUNDO/src/eva1_1_hola_mundo/EVA1_1_HOLA_MUNDO.java
@@ -0,0 +1,10 @@
+package eva1_1_hola_mundo;
+
+public class EVA1_1_HOLA_MUNDO {
+    
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO!!");
+        System.out.println("FUNDAMENTOS DE PROGRAMACION");
+    }
+    
+}
